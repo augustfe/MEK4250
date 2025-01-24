30d366b4a9bd2a9161940db0cf670f7b9299a876
August Femtehjell <august.femtehjell@gmail.com>
2025-01-24 16:11:43 +0100

    Wrote up exercise 1.1

--- exercises/exercise_1_1.py
+++ exercises/exercise_1_1.py
@@ -32,18 +32,51 @@ def u_N(x: np.ndarray, N: int) -> np.ndarray:
 def compute_error(N: int, ord: str | int) -> float:
     x = np.linspace(0, 1, 1000)
 
-    import matplotlib.pyplot as plt
-
-    plt.plot(x, true_u(x), label="True solution")
-    plt.plot(x, u_N(x, N), label=f"Approximation with N={N}")
-    plt.legend()
-    plt.show()
-
     return np.linalg.norm(true_u(x) - u_N(x, N), ord=ord)
 
 
+def plot(N: int) -> None:
+    from pathlib import Path
+
+    import matplotlib.pyplot as plt
+
+    savedir = Path(__file__).parents[1] / "figs"
+
+    assert savedir.exists()
+
+    plt.figure(figsize=(4, 3))
+    x = np.linspace(0, 1, 250)
+    plt.plot(x, true_u(x), label="True $u$")
+    plt.plot(x, u_N(x, N), label=f"$u_{{{N}}}$")
+    plt.legend()
+    plt.title(f"Approximation of $u$ with $N = {N}$")
+    plt.xlabel("$x$")
+
+    plt.savefig(savedir / "exercise_1_1.pdf", bbox_inches="tight")
+    plt.clf()
+
+    plt.figure(figsize=(4, 3))
+    coeffs = compute_coeffs(N)
+    plt.scatter(np.arange(1, N + 1), np.abs(coeffs), marker="x")
+    plt.title(f"Coefficients for $N = {N}$")
+    plt.xlabel("$n$")
+    plt.ylabel("$u_n$")
+    plt.yscale("log")
+    plt.savefig(savedir / "exercise_1_1_coeffs.pdf", bbox_inches="tight")
+    plt.clf()
+
+
 if __name__ == "__main__":
+    import pandas as pd
+
     Ns = [10, 20, 40]
-    for N in Ns:
-        print(f"Error for N = {N}: {compute_error(N, ord=2)}")
-        print(f"Error for N = {N}: {compute_error(N, ord=np.inf)}")
+
+    results = np.zeros((len(Ns), 2))
+    for i, N in enumerate(Ns):
+        results[i, 0] = compute_error(N, ord=2)
+        results[i, 1] = compute_error(N, ord=np.inf)
+
+    df = pd.DataFrame(results, index=Ns, columns=[r"$L_2$", r"$L_\infty$"])
+    print(df.to_latex())
+
+    plot(Ns[-1])
